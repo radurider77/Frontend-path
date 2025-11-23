0165bd804462c65b49a1eb58c4f3f8820a45b71e
radurider77 <radurider77@gmail.com>
2025-11-23 21:32:43 +0200

    Homework add HTML elements

---
+++ Homework/java.js
@@ -0,0 +1,7 @@
+const article = document.querySelector("#electric-cars");
+// The following would also work:
+// const article = document.getElementById("electric-cars")
+
+article.dataset.columns; // "3"
+article.dataset.indexNumber; // "12314"
+article.dataset.parent; // "cars"
