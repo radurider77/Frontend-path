11818befb934217f48bd8909284f9e2b64b17364
radurider77 <radurider77@gmail.com>
2025-12-03 12:56:52 +0200

    add semantic tags

--- Homework/java.js
+++ Homework/java.js
@@ -5,3 +5,10 @@ const article = document.querySelector("#electric-cars");
 article.dataset.columns; // "3"
 article.dataset.indexNumber; // "12314"
 article.dataset.parent; // "cars"
+console.log(article.dataset.columns);
+
+const button = document.querySelector("#electric-cars button");
+
+button.addEventListener("click", function () {
+  console.log(article.dataset.model, article.dataset.car);
+});
